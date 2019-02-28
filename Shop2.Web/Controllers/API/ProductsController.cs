6db77b098e635371a79474b14011d3af68aa15dc
david <davidknife69@gmail.com>
2019-02-28 14:11:15 -0500

    Add API and images

---
+++ Shop2.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop2.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop2.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
---
+++ Shop2.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop2.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+    using Shop2.Web.Data.Entities;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
